42d60a004462938237ec0029561ab9dedd78694d
Jason <jasonmarble@gmail.com>
2021-10-28 15:19:31 -0600

    adds core server files

---
+++ server/routes/allProjects.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const Project = require('../../database/models/Project');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Project.find();
+        res.json(projects);
+    } catch (err) {
+        res.send(`Error: ${err}`);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.id);
+        res.json(project);
+    } catch (err) {
+        res.send(`Error: ${err}`);
+    }
+});
+
+router.post('/', async (req, res) => {
+    // const { req.body } = req.body;
+    const project = new Project({
+        item: req.body.item,
+        info: req.body.info,
+        quantity: req.body.quantity,
+        selected: req.body.selected,
+        sellby: req.body.sellby,
+    });
+
+    try {
+        const write = await project.save();
+        res.send(write);
+    } catch (err) {
+        res.send(`Error: ${err}`);
+    }
+});
+
+router.post('/:id', async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.id);
+        /* find out what's been changed and assign here. ie. project[target] */
+        const update = await project.save();
+        res.json(update);
+    } catch (err) {
+        res.send(`Error: ${err}`);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.id);
+        const removed = await project.remove();
+        res.json(removed);
+    } catch (err) {
+        res.send(`Error: ${err}`);
+    }
+});
+
+module.exports = router;
