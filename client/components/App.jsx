2ec1e9a22f610f3eff3ded7965a0ace8a175046c
Jason <jasonmarble@gmail.com>
2021-10-28 15:42:20 -0600

    changes .gitignore rules

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 export default function App() {
     return (
         <div>
-            <h1>MERN starter!</h1>
-            <h2>Project Planner</h2>
+            <h1>Project Planner</h1>
         </div>
     )
 };
