fc78d82bed02e4d237a82b831583da6bba4752bf
Jason <jasonmarble@gmail.com>
2021-10-28 21:00:42 -0600

    trims comments and adds public directory

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import ReactDOM from 'react-dom';
-// eslint-disable-next-line import/extensions
 import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,10 @@ require('dotenv/config');
 const path = require('path');
 
 module.exports = {
-  // entry point for this application will be in client dir w/in index.js
+  mode: 'development',
   entry: path.resolve(__dirname, 'client', 'index.jsx'),
   output: {
-    // output will be stored in dist dir
     path: path.resolve(__dirname, 'dist'),
-    // naming bundled file
     filename: 'bundle.js',
   },
   devServer: {
@@ -21,17 +19,13 @@ module.exports = {
   module: {
     rules: [
       {
-        // match & resolve js & jsx files w/ their corresponding loader
         test: /\.(js|jsx)$/,
-        // ignore transpiling node_modules
         exclude: /node_modules/,
-        // use the babel loader for transpiling code
         use: {
           loader: 'babel-loader',
         },
       },
       {
-        // pure CSS (i.e. w/o CSS modules)
         test: /\.css$/i,
         exclude: /\.module\.css$/i,
         use: ['style-loader', 'css-loader'],
