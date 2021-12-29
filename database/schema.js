46b0e84c423fe8fac96eb8ad7cf9c5b6ef8ce737
Jason <jasonmarble@gmail.com>
2021-12-29 13:01:04 -0700

    updates schema

--- database/schema.js
+++ database/schema.js
@@ -3,9 +3,51 @@ const mongoose = require('mongoose');
 // create a new schema
 const { Schema } = mongoose;
 const projectSchema = new Schema({
-  item: String,
+  title: String,
   info: String,
-  selected: Boolean,
+  taskSection: [
+    {
+      title: String,
+      tasks: [
+        {
+          title: String,
+          info: String,
+          complete: Boolean,
+          subtasks: [
+            {
+              title: String,
+              info: String,
+              complete: Boolean,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  calendarEvents: [
+    {
+      title: String,
+      date: Date,
+      event: {
+        info: String,
+        endsOn: Date,
+        allDay: Boolean,
+      },
+    },
+  ],
+  resources: [
+    {
+      title: String,
+      info: String,
+      link: String,
+    },
+  ],
+  notes: [
+    {
+      title: String,
+      info: String,
+    },
+  ],
 });
 
 module.exports = projectSchema;
