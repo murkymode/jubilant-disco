ca7cf03734a127d20d8b99cd5f5e63e70014d7c9
Jason <jasonmarble@gmail.com>
2022-01-11 14:51:47 -0700

    adds component for task construction

---
+++ client/src/components/services/project_hud/AddTask.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function AddTask({ handleClick }) {
+  return (
+    <div
+      style={{
+        minHeight: '60px',
+        maxHeight: '60px',
+        fontSize: '4em',
+      }}
+    >
+      ✚
+    </div>
+  );
+}
