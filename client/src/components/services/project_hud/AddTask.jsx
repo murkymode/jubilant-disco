062e3b0186386ea79a824ffd5742fd89b4746a7e
Jason <jasonmarble@gmail.com>
2022-01-13 12:51:31 -0700

    adds hover effect

--- client/src/components/services/project_hud/AddTask.jsx
+++ client/src/components/services/project_hud/AddTask.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Box } from '@mui/material';
 
-export default function AddTask({ handleClick }) {
+export default function AddTask({ addTask }) {
   return (
-    <div
-      style={{
+    <Box
+      sx={{
         minHeight: '60px',
         maxHeight: '60px',
         fontSize: '4em',
+        '&:hover': {
+          opacity: [0.9, 0.8, 0.7],
+          cursor: 'pointer',
+        },
       }}
+      onClick={addTask}
     >
       ✚
-    </div>
+    </Box>
   );
 }
