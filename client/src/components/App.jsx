4e33976146fb97b4a97bd379dc1fa1c6a60dfb6b
Jason <jasonmarble@gmail.com>
2022-01-14 14:31:39 -0700

    moves data fetching to app level

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,24 +7,45 @@ import {
   Route, Switch,
 } from 'react-router-dom';
 
+/* data fetching */
+import {
+  QueryClient, QueryClientProvider, useQuery,
+} from 'react-query';
+
 /* pages */
 import Splash from './pages/Splash';
 import Home from './pages/Home';
 
-export default function App() {
+const queryClient = new QueryClient();
+
+export default function Page() {
   return (
-    <>
-      <CssBaseline />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Splash />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   );
+  function App() {
+    const {
+      isLoading, error, data,
+    } = useQuery('fetchProjects', () => fetch('/api/projects')
+      .then((res) => res.json()));
+
+    if (isLoading) return 'Loading Projects...';
+    if (error) return `Aww, an error: ${error.message}`;
+    return (
+      <>
+        <CssBaseline />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Splash projects={data} />
+            </Route>
+            <Route path="/home">
+              <Home projects={data} />
+            </Route>
+          </Switch>
+        </Router>
+      </>
+    );
+  }
 }
