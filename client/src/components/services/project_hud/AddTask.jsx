5b404c952cb0c6084826e0259562f442f2d4f703
Jason <jasonmarble@gmail.com>
2022-01-14 19:59:52 -0700

    styles for now

--- client/src/components/services/project_hud/AddTask.jsx
+++ client/src/components/services/project_hud/AddTask.jsx
@@ -5,12 +5,13 @@ export default function AddTask({ addTask }) {
   return (
     <Box
       sx={{
-        minHeight: '60px',
-        maxHeight: '60px',
+        // minHeight: '60px',
+        // maxHeight: '60px',
         fontSize: '4em',
         '&:hover': {
           opacity: [0.9, 0.8, 0.7],
           cursor: 'pointer',
+          color: 'green',
         },
       }}
       onClick={addTask}
