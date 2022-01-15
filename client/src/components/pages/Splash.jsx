72e4eb414c08bea42d6d8eb8df7717f4560f17b5
Jason <jasonmarble@gmail.com>
2022-01-14 19:58:36 -0700

    moves fetching

--- client/src/components/pages/Splash.jsx
+++ client/src/components/pages/Splash.jsx
@@ -1,61 +1,47 @@
 /* eslint-disable no-underscore-dangle */
-import * as React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
-import {
-  QueryClient, QueryClientProvider, useQuery,
-} from 'react-query';
 
 import {
   Box, Paper, Stack, Button,
 } from '@mui/material';
 
 import theme from '../theme';
+import Context from '../context';
 
 const { main } = theme.palette.secondary;
 
-const queryClient = new QueryClient();
+export default function Splash({ projects }) {
+  /* sets current project id in context */
+  const { handleID } = useContext(Context);
+  const setID = (e) => {
+    const { id } = e.target.parentNode;
+    handleID(id);
+  };
 
-export default function Page() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Splash />
-    </QueryClientProvider>
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        '& > :not(style)': {
+          backgroundColor: main,
+          m: 1,
+          width: '60vw',
+          height: '40vh',
+        },
+      }}
+    >
+      <Paper elevation={4}>
+        <Stack>
+          {projects.map((project) => (
+            <Button onClick={setID} id={project._id} key={project._id}>
+              <Link to="/home">{project.title}</Link>
+            </Button>
+          ))}
+        </Stack>
+      </Paper>
+    </Box>
   );
-
-  function Splash() {
-    const {
-      isLoading, error, data,
-    } = useQuery('fetchProjects', () => fetch('/api/projects')
-      .then((res) => res.json()));
-
-    if (isLoading) return 'Loading Projects...';
-    if (error) return `Aww, an error: ${error.message}`;
-
-    return (
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          '& > :not(style)': {
-            backgroundColor: main,
-            m: 1,
-            width: '60vw',
-            height: '40vh',
-          },
-        }}
-      >
-        <Paper elevation={4}>
-          <Stack>
-            {data.map((project) => (
-              <Button key={project._id}>
-                <Link to="/home">{project.title}</Link>
-              </Button>
-            ))}
-          </Stack>
-        </Paper>
-      </Box>
-    );
-  }
 }
