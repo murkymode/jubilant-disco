8e543a6ed112935c55bb887c2c988908a3cdb485
Jason <jasonmarble@gmail.com>
2022-01-15 17:22:12 -0700

    POST button working

--- client/src/components/services/add_project/index.jsx
+++ client/src/components/services/add_project/index.jsx
@@ -1,10 +1,29 @@
 import * as React from 'react';
 import axios from 'axios';
 import {
-  Paper, TextField, Box,
+  Paper, TextField, Box, Button,
 } from '@mui/material';
 
 export default function AddProject() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const info = e.target[2].value;
+
+    const request = {
+      title,
+      info,
+    };
+
+    axios.post('/api/projects', request)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <Box>
       <Paper
@@ -15,27 +34,34 @@ export default function AddProject() {
           justifyContent: 'space-evenly',
           '& > :not(style)': {
             m: 1,
-            // width: 128,
-            // height: 128,
           },
         }}
       >
-        <TextField
-          id="search-input"
-          variant="outlined"
-          label="Project Title"
-          InputLabelProps={{
-            style: { color: 'white' },
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
           }}
-        />
-        <TextField
-          id="search-input"
-          variant="outlined"
-          label="Project Description"
-          InputLabelProps={{
-            style: { color: 'white' },
-          }}
-        />
+        >
+          <TextField
+            id="search-input"
+            variant="outlined"
+            label="Project Title"
+            InputLabelProps={{
+              style: { color: 'white' },
+            }}
+          />
+          <TextField
+            id="search-input"
+            variant="outlined"
+            label="Project Description"
+            InputLabelProps={{
+              style: { color: 'white' },
+            }}
+          />
+          <button style={{ fontSize: '4em', padding: '0.3em' }} type="submit">+</button>
+        </form>
       </Paper>
     </Box>
   );
