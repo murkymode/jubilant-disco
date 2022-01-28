713ecb754bbc515ea18c856ac15e7c30820a2a03
Jason <jasonmarble@gmail.com>
2022-01-28 16:02:25 -0700

    removes static column data

--- client/src/components/services/project_view/board_view/index.jsx
+++ client/src/components/services/project_view/board_view/index.jsx
@@ -3,38 +3,7 @@ import React, { useState } from 'react';
 import Column from './task_column';
 
 export default function Taskboard() {
-  const [columns, setColumns] = useState([
-    {
-      id: 1,
-      title: 'column 1',
-      tasks: [
-        {
-          id: 4,
-          title: 'thing 1',
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: 'column 2',
-      tasks: [
-        {
-          id: 5,
-          title: 'thing 2',
-        },
-      ],
-    },
-    {
-      id: 3,
-      title: 'column 3',
-      tasks: [
-        {
-          id: 6,
-          title: 'thing 3',
-        },
-      ],
-    },
-  ]);
+  const [columns, setColumns] = useState([]);
   return (
     <div
       style={{
