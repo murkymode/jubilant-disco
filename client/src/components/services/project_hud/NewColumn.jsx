39633f90663a7bde119fc50d95fcf703232e03b4
Jason <jasonmarble@gmail.com>
2022-02-03 14:11:24 -0700

    add new column working

--- client/src/components/services/project_hud/NewColumn.jsx
+++ client/src/components/services/project_hud/NewColumn.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
+import axios from 'axios';
 
 export default function NewColumn() {
-  const [formData, setFormData] = useState({
-    title: '',
-    info: '',
-  });
   const handleSubmit = (e) => {
-    e.preventDefault();
+    const currentID = window.sessionStorage.getItem('currentProject');
     const title = e.target[0].value;
     const info = e.target[1].value;
-    setFormData({
-      title,
-      info,
-    });
+
+    axios.post('/api/projects/addColumn', { id: currentID, title, info })
+      .then((res) => res.send())
+      .catch((err) => console.log(err));
   };
 
   return (
