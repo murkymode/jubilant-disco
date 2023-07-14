d808e05d1a76993f530de76e05ada77bfa46e883
murkymode <jasonmarble@gmail.com>
2023-07-13 21:05:34 -0600

    inits Quill for simple note taker

--- client/src/components/services/project_view/notes_view/index.jsx
+++ client/src/components/services/project_view/notes_view/index.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export default function Notes() {
+  const [value, setValue] = useState('');
+
   return (
-    <div>Hello from Notes</div>
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={setValue}
+      style={{
+        width: '33vw',
+        height: '66vh',
+        margin: '11px',
+      }}
+    />
   );
 }
